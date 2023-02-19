c6917c126360f46f6c76061b907de686730fa60f
Ian Henderson <ian.henderson1215@gmail.com>
2023-02-19 14:07:05 -0800

    first commit, created main and hero classes

--- src/Main/Hero.java
+++ src/Main/Hero.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Hero {
+package Main;
+
+public class Hero {
+
+    //attributes
+    private String firstName;
+    private String lastName;
+    private int healthPoints = 200;
+
+//Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    //Setters
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    //Constructors
+
+
+    public Hero(String firstName, String lastName, int healthPoints) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.healthPoints = healthPoints;
+    }
 }
--- src/Main/MainApp.java
+++ src/Main/MainApp.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class MainApp {
+package Main;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Hero Ian = new Hero("Ian", "Henderson", 150);
+
+
+
+
+
+    }
 }
